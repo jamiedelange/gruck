b4f1c59c41d8a900ded72bbdd4df096e4753ff9c
jelly octane <jamie.de.lange@mac.com>
2023-02-09 13:33:22 -0700

    added second screen, fixed bug

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const c = document.getElementById("theCanvas")
+const c2 = document.getElementById("theCanvas2")
 const ctx = c.getContext("2d")
+const ctx2 = c2.getContext("2d")
 ctx.fillStyle = "#FF70AB"
+ctx.fillRect(0, 0, 256, 256)
+ctx.fillStyle = "#57BCFF"
 ctx.fillRect(0, 0, 256, 256)
 
 const triangle = [[73, 100, 200], [100, 115, 112], [125, 131, 140]]
 var animCount = 0
-var cameraPos = [0, 0, 0]
+var cameraPos = [128, 128, 0]
 var camAngle = 0
 const projPlaneDist = 64
 
 function convertToRadians(degrees) {
-    return (degrees * Math.PI) / 180
+    let rads = (degrees * Math.PI) / 180
+    console.log("Camera position: " + cameraPos)
+    console.log("camera angle radians: " + rads)
+    return rads
 }
 
 function getPlanePoint() {
-    x1 = projPlaneDist * Math.cos(convertToRadians(camAngle))
+    let rads = convertToRadians(camAngle)
+    x1 = projPlaneDist * Math.sin(rads)
     y1 = 0
-    z1 = projPlaneDist * Math.sin(convertToRadians(camAngle))
-    return x1, y1, z1
+    z1 = projPlaneDist * Math.cos(rads)
+    console.log("point on plane: " + [x1, y1, z1])
+    return [x1, y1, z1]
 }
 
 function calcNormalVector(planePoint) {
-    x1, y1, z1 = planePoint
-    return [x1 - cameraPos[0], 0, z1 - cameraPos[2]]
+    console.log(planePoint)
+    let normVec = [planePoint[0] - cameraPos[0], planePoint[1] - cameraPos[1], planePoint[2] - cameraPos[2]]
+    console.log("Normal vector: " + normVec)
+    return normVec
 }
 
 function getZprime(z) {
@@ -34,6 +45,8 @@ function getZprime(z) {
 
     let d = (a * planePoint[0]) + (a * planePoint[1]) + (a * planePoint[2])
     let k = (d - a*z[0] - b*z[1] - c*z[2]) / (a*a + b*b + c*c)
+    console.log("d: " + d)
+    console.log("k: " + k)
     return [z[0] + k*a, z[1] + k*b, z[2] + k*c]
 }
 
@@ -48,7 +61,7 @@ function moveUp() {
     // }
     cameraPos[0] += 4
     cameraPos[2] += 4
-    camAngle += 7
+    camAngle -= 7
 }
 
 function moveBack() {
@@ -60,7 +73,7 @@ function moveBack() {
     // }
     cameraPos[0] -= 4
     cameraPos[2] -= 4
-    camAngle -= 7
+    camAngle += 7
 }
 
 function drawTriangle() {
@@ -68,6 +81,8 @@ function drawTriangle() {
     ctx.fillRect(0, 0, 256, 256)
 
     projectedTriangle = [getZprime(triangle[0]), getZprime(triangle[1]), getZprime(triangle[2])]
+    console.log("triangle: " + projectedTriangle)
+    console.log("projected triangle: " + projectedTriangle)
 
     ctx.fillStyle = "#FFFFFF"
     ctx.beginPath()
@@ -80,15 +95,57 @@ function drawTriangle() {
     ctx.stroke()
 }
 
-setInterval(() => {
-    if (animCount < 10) {
+function drawOtherView() {
+    ctx2.fillStyle = "#57BCFF"
+    ctx2.fillRect(0, 0, 256, 256)
+
+    ctx2.fillStyle = "#FFFFFF"
+    ctx2.beginPath()
+    ctx2.moveTo(triangle[0][0], triangle[0][2])
+    ctx2.lineTo(triangle[1][0], triangle[1][2])
+    ctx2.stroke()
+    ctx2.lineTo(triangle[2][0], triangle[2][2])
+    ctx2.stroke()
+    ctx2.lineTo(triangle[0][0], triangle[0][2])
+    ctx2.stroke()
+
+    ctx2.moveTo(cameraPos[0], cameraPos[2])
+    ctx2.fillStyle = "#FFFFFF"
+    ctx2.beginPath()
+    ctx2.arc(cameraPos[0], cameraPos[2], 6, 0, 2 * Math.PI, false);
+    ctx2.fill()
+    ctx2.stroke()
+
+    ctx2.beginPath()
+    ctx2.moveTo(cameraPos[0], cameraPos[2])
+    let planePoint = getPlanePoint()
+    ctx2.lineTo(planePoint[0], planePoint[2])
+    console.log(planePoint[0] + " , " + planePoint[2])
+    ctx2.stroke()
+}
+
+drawTriangle()
+drawOtherView()
+console.log("-------------------------------------------------------")
+
+// setInterval(() => {
+//     // if (animCount < 10) {
+//     //     moveUp()
+//     //     animCount++
+//     // } else if (animCount < 20) {
+//     //     moveBack()
+//     //     animCount++
+//     // } else {
+//     //     animCount = 0
+//     // }
+//     drawTriangle()
+// }, 1000)
+
+document.addEventListener('keydown', function(event) {
+    if(event.key == "ArrowDown") {
         moveUp()
-        animCount++
-    } else if (animCount < 20) {
-        moveBack()
-        animCount++
-    } else {
-        animCount = 0
+        drawTriangle()
+        console.log("-------------------------------------------------------")
+        drawOtherView()
     }
-    drawTriangle()
-}, 50)
+});
